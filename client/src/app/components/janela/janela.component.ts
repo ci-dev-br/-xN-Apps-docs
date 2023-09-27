cb1a3f8d1d72427dfe1a09f8639994e828ce2610
plhx <plhx@plhx.com.br>
2023-09-26 21:39:37 -0300

    #12 - implementação simples de menu de ações me janela;

--- client/src/app/components/janela/janela.component.ts
+++ client/src/app/components/janela/janela.component.ts
@@ -1,5 +1,13 @@
-import { Component, Inject, Input, TemplateRef, Type, ViewChild } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, Injector, Input, TemplateRef, Type, ViewChild } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface IItemMenu {
+  icon?: string;
+  label?: string;
+  path?: string;
+  children?: IItemMenu[];
+  onClick?: () => void;
+}
 
 @Component({
   selector: 'ci-janela',
@@ -7,20 +15,24 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./janela.component.scss']
 })
 export class JanelaComponent {
-  menu = [
+  menu: IItemMenu[] = [
     // { icon: 'visibility' },
     // { icon: 'thumb_up' },
     // { icon: 'share' },
     // { icon: 'more_horiz' },
     { icon: 'done_all' },
-    { icon: 'close' },
+    { icon: 'close', onClick: () => this.close() },
   ];
   @Input()
   component?: Type<any>;
   @ViewChild('componentTmplate')
   componentTmplate?: any;
   constructor(
+    private readonly ref: MatDialogRef<JanelaComponent>,
     @Inject(MAT_DIALOG_DATA)
     private data: any,
   ) { }
+  close() {
+    this.ref.close();
+  }
 }
