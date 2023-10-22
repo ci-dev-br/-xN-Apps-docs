c6c4b02813496eae8d955dd33fad269e922a7f56
plhx <plhx@plhx.com.br>
2023-10-22 18:41:56 -0300

    correção em entidade de mensagem e conversa;

--- server/src/messager/model/conversation.entity.ts
+++ server/src/messager/model/conversation.entity.ts
@@ -1,6 +1,11 @@
-import { Entity } from "typeorm";
+import { User } from "src/auth/models/user.entity";
+import { Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Conversation {
-
+    @PrimaryGeneratedColumn('uuid')
+    id?: string;
+    @JoinTable()
+    @ManyToMany(() => User)
+    participants: User[];
 }
