cfc98960946653f912621cda332f2da86d67a0d6
plhx <plhx@plhx.com.br>
2023-10-28 17:18:45 -0300

    Implementação de Cell Renderer para componentes de Grid;

---
+++ client/src/app/components/grid/cell-renderer/cell-renderer.component.ts
@@ -0,0 +1,52 @@
+import { Component, Injector, Input, TemplateRef, Type, ViewChild, ViewContainerRef } from "@angular/core";
+
+export interface IColumns {
+    headerName: string;
+    propertyName: string;
+    rendererType?: string;
+    component?: any;
+    componentType?: Type<any>
+    hide?: boolean;
+}
+@Component({
+    selector: 'px-cell-renderer',
+    template: `
+    <ng-container *ngIf="column && !column?.component">
+        {{data[column.propertyName]}}
+    </ng-container>
+    <ng-container *ngIf="column && !!column?.component">
+        <ng-template #teste>
+            {{data[column.propertyName]}}
+        </ng-template>
+        <ng-container *ngComponentOutlet="column.component; content: componentContent">
+        </ng-container>
+    </ng-container>
+    `,
+    styles: [
+        `:host{display:contents}`
+    ]
+})
+export class TextCellRenderer {
+    constructor(
+        private readonly vcr: ViewContainerRef,
+    ) { }
+    private _teste!: TemplateRef<any>;
+    public get teste(): TemplateRef<any> {
+        return this._teste;
+    }
+    @ViewChild('teste', { static: false })
+    public set teste(value: TemplateRef<any>) {
+        this._teste = value;
+        this.componentContent = [
+            this.vcr.createEmbeddedView(this.teste).rootNodes,
+        ];
+    }
+    @Input()
+    data?: any;
+    @Input()
+    column?: IColumns;
+    componentContent?: any[][];
+    ngOnInit() {
+
+    }
+}
