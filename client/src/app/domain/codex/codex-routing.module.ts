f2205131ac1b63fc4c95d42b51ee7ac0481ba636
plhx <plhx@plhx.com.br>
2023-10-27 22:59:35 -0300

    ajustes gerais

--- client/src/app/domain/codex/codex-routing.module.ts
+++ client/src/app/domain/codex/codex-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CodeEditorComponent } from './code-editor/code-editor.component';
 const routes: Routes = [
-  { path: '', component: CodeEditorComponent, }
+  {
+    path: '', component: CodeEditorComponent
+  }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
