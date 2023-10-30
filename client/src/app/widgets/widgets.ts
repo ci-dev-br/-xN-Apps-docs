36e218783a5cc83db16b8bee2d7a6b57a63e5c51
plhx <plhx@plhx.com.br>
2023-10-30 02:13:23 -0300

    adicionado interface para configuração de Widget;

--- client/src/app/widgets/widgets.ts
+++ client/src/app/widgets/widgets.ts
@@ -1,19 +1,27 @@
 import { Type } from "@angular/core";
 import { IFrameWidget, IFrameWidgetModule } from "./iframe/iframe.widget";
 
+export interface IControl {
+    type?: string;
+}
+
 export interface IWidget {
     title?: string;
     description?: string;
     tags?: string[];
     component?: Type<any>;
     module?: Type<any>;
+    settings?: { [control: string]: IControl };
 }
 
 export const Widgets: IWidget[] = [
     {
         title: "IFrame Widget",
-        tags: [],
         description: 'Página externa a partir de uma URL.',
+        tags: ['Elementos'],
+        settings: {
+            url: { type: 'text' }
+        },
         module: IFrameWidgetModule, component: IFrameWidget
     },
 ]
