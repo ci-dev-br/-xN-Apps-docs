2e3df03429ea0a759621df1002185ea7b80cc7b1
plhx <plhx@plhx.com.br>
2023-11-02 21:32:58 -0300

    #47: ajuste em rota para adicionar outros recursos além de edição de código;

--- client/src/app/domain/codex/codex-routing.module.ts
+++ client/src/app/domain/codex/codex-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CodeEditorComponent } from './code-editor/code-editor.component';
+import { IMenu } from 'src/app/core/i-menu';
 const routes: Routes = [
   {
-    path: '', component: CodeEditorComponent,
+    path: '', children: [
+      {
+        path: '', component: CodeEditorComponent,
+        data: {
+          name: 'CodeX',
+          icon: 'code',
+        }
+      }
+    ],
     data: {
-      name: 'CodeX',
-      icon: 'code'
+      name: 'IDE Integrada',
+      menu: [
+        { label: 'Código', route: '', icon: 'code' }
+      ] as IMenu
     }
   }
 ];
