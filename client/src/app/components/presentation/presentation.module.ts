fac63c765268ea76c682b2676c2a7d2bbf6a2804
plhx <plhx@plhx.com.br>
2023-11-04 11:26:22 -0300

    adicionado componente `presentation` para tela dash inicial para aplicações;

---
+++ client/src/app/components/presentation/presentation.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PresentationComponent } from './presentation.component';
+
+
+
+@NgModule({
+  declarations: [
+    PresentationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PresentationModule { }
