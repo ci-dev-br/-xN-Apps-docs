de687e468621c23e1265980f73e9dded53487a1b
plhx <plhx@plhx.com.br>
2023-11-11 19:33:15 -0300

    Alterado bank account type para enum

--- server/src/prolabore/models/conta-bancaria.entity.ts
+++ server/src/prolabore/models/conta-bancaria.entity.ts
@@ -4,29 +4,33 @@ import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class ContaBancaria {
-    @ApiProperty({ nullable: false})
+    @ApiProperty({ nullable: false })
     @PrimaryGeneratedColumn('uuid')
     id: string;
-    @ApiProperty({ nullable: false, required: true})
+    @ApiProperty({ nullable: false, required: true })
     @ManyToOne(() => Banco)
     @JoinTable()
     bank: Banco;
-    @ApiProperty({ nullable: false, required: true})
-    @Column({ nullable: false, unique: true})
+    @ApiProperty({ nullable: false, required: true })
+    @Column({ nullable: false, unique: true })
     agency: string;
-    @ApiProperty({ nullable: false})
-    @Column({ nullable: false})
+    @ApiProperty({ nullable: false })
+    @Column({ nullable: false })
     ownerName: string;
-    @ApiProperty({ nullable: false})
-    @Column({ nullable: false})
+    @ApiProperty({ nullable: false })
+    @Column({ nullable: false })
     cpfCnpj: string;
-    @ApiProperty({ nullable: false})
-    @Column({ nullable: false, unique: true})
+    @ApiProperty({ nullable: false })
+    @Column({ nullable: false, unique: true })
     account: string;
-    @ApiProperty({ nullable: false})
-    @Column({ nullable: false, unique: true})
+    @ApiProperty({ nullable: false })
+    @Column({ nullable: false, unique: true })
     accountDigit: string;
-    @ApiProperty({ nullable: false})
-    @Column({ nullable: false})
+    @ApiProperty({ nullable: false })
+    @Column({
+        nullable: false, enum: [
+            'CONTA_CORRENTE', 'CONTA_POUPANCA'
+        ]
+    })
     bankAccountType: "CONTA_CORRENTE" | "CONTA_POUPANCA";
 }
