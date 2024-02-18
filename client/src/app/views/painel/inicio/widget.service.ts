40818c5ab46b1ba370a2b9d25ed26eb846a1a77a
plhx <plhx@plhx.com.br>
2024-02-18 17:34:05 -0300

    Widget Service;

---
+++ client/src/app/views/painel/inicio/widget.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { IWidget } from "src/app/widgets/i-widget";
+
+@Injectable()
+export class WidgetService {
+    constructor(
+
+    ) { }
+    async adicionarWidget(
+        settings: any,
+        widget: IWidget,
+    ) { }
+}
