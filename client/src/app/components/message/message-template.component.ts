c23c6d234e465a8f49ec250262c74f3d1deed8fb
pLinho <paulorettamozo@gmail.com>
2024-02-29 02:03:34 -0300

    ++

--- client/src/app/components/message/message-template.component.ts
+++ client/src/app/components/message/message-template.component.ts
@@ -2,9 +2,8 @@ import { Component, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
-    selector: 'ci-message-template', template: `
-    @if(data?.template){<ng-container *ngTemplateOutlet="data.template" ></ng-container>}
-` })
+    selector: 'ci-message-template', template: `@if(data?.template){<ng-container *ngTemplateOutlet="data.template" ></ng-container>}`
+})
 export class MessageTemplateComponent {
     constructor(
         @Inject(MAT_DIALOG_DATA)
