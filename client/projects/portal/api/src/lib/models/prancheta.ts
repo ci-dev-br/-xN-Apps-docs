e00d5fcb1dca1bc104a2ea403150aab5b9c5169b
pLinho <paulorettamozo@gmail.com>
2024-03-14 00:58:28 -0300

    removido id em prancheta, motivo, já estende FullAuditedEntity em que possui internald;

--- client/projects/portal/api/src/lib/models/prancheta.ts
+++ client/projects/portal/api/src/lib/models/prancheta.ts
@@ -6,7 +6,6 @@ export interface Prancheta {
   cards?: Card | null;
   createdAt?: string | null;
   createdBy?: ChaveAcesso | null;
-  id?: string | null;
   internalId?: string | null;
   lastModifiedAt?: string | null;
   lastModifiedBy?: ChaveAcesso | null;
