ae5947897afeb63b9a6e2139e5aeb9f9cb7c3d73
pLinho <paulorettamozo@gmail.com>
2024-06-04 23:37:15 -0300

    #70: Implementação da barra superior;

--- apps/src/app/painel/painel-routing.module.ts
+++ apps/src/app/painel/painel-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { PainelComponent } from './painel.component';
 
 const routes: Routes = [
-  { path: '', component: PainelComponent, }
+  {
+    path: '', component: PainelComponent, children: [
+      // { path: '', loadChildren: () => import('./../domain') },
+    ]
+  }
 ];
 
 @NgModule({
--- apps/src/app/painel/painel.module.ts
+++ apps/src/app/painel/painel.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PainelRoutingModule } from './painel-routing.module';
-
-
 @NgModule({
   declarations: [],
   imports: [
