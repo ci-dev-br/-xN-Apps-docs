c3574a096180f16470c0720a30dd9d3a9ccc0cb1
pLinho <paulorettamozo@gmail.com>
2024-06-07 03:38:32 -0300

    Adicionado Icones; Adicionado Material Symbols Rounded; Adicionado cor aleatória para o menu de aplicações em Apps;

--- apps/src/app/app.component.ts
+++ apps/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'ci-root',
   standalone: true,
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'apps';
+  constructor(
+    private readonly matIconReg: MatIconRegistry,
+
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.matIconReg.setDefaultFontSetClass('material-symbols-sharp')
+  }
+
 }
--- apps/src/app/painel/apps/apps.component.ts
+++ apps/src/app/painel/apps/apps.component.ts
@@ -16,33 +16,40 @@ import { Application } from '@ci/portal-api';
   styleUrl: './apps.component.scss'
 })
 export class AppsComponent {
-  apps?: Application[] = [
+
+
+  apps?: any[] = [
     // { url: '', name: 'Gerencial', roles: ['MASTER'], icon: 'manager' },
     // { url: '', name: 'Produtos', roles: ['MASTER'], icon: 'manager' },
     // { url: '', name: 'Vendas', roles: ['MASTER'], icon: 'manager' },
     // { url: '', name: 'XCode', roles: ['MASTER'], icon: 'manager' },
     // { url: '', name: 'Arquivos', roles: ['MASTER'], icon: 'manager' },
-    { url: '../Arquivos', name: 'Arquivos', roles: ['MASTER'], icon: undefined },
-    { url: '../Cadastros', name: 'Cadastros', roles: ['MASTER'], icon: undefined },
-    { url: '../Codex', name: 'Codex', roles: ['MASTER'], icon: undefined },
-    { url: '../DevTools', name: 'DevTools', roles: ['MASTER'], icon: undefined },
-    { url: '../Dynamic', name: 'Dynamic', roles: ['MASTER'], icon: undefined },
-    { url: '../Financeiro', name: 'Financeiro', roles: ['MASTER'], icon: undefined },
-    { url: '../Formularios', name: 'Formularios', roles: ['MASTER'], icon: undefined },
-    { url: '../Gerencial', name: 'Gerencial', roles: ['MASTER'], icon: undefined },
-    { url: '../Icons', name: 'Icons', roles: ['MASTER'], icon: undefined },
-    { url: '../Imersao', name: 'Imersao', roles: ['MASTER'], icon: undefined },
-    { url: '../Infra', name: 'Infra', roles: ['MASTER'], icon: undefined },
-    { url: '../Instalacao', name: 'Instalacao', roles: ['MASTER'], icon: undefined },
-    { url: '../LowCode', name: 'LowCode', roles: ['MASTER'], icon: undefined },
-    { url: '../Mensagens', name: 'Mensagens', roles: ['MASTER'], icon: undefined },
-    { url: '../Organizacao', name: 'Organizacao', roles: ['MASTER'], icon: undefined },
-    { url: '../Produtos', name: 'Produtos', roles: ['MASTER'], icon: undefined },
-    { url: '../Profile', name: 'Profile', roles: ['MASTER'], icon: undefined },
-    { url: '../Projetos', name: 'Projetos', roles: ['MASTER'], icon: undefined },
-    { url: '../SEO', name: 'SEO', roles: ['MASTER'], icon: undefined },
-    { url: '../Threejs', name: 'Threejs', roles: ['MASTER'], icon: undefined },
-    { url: '../Treinamento', name: 'Treinamento', roles: ['MASTER'], icon: undefined },
-    { url: '../Vendas', name: 'Vendas', roles: ['MASTER'], icon: undefined },
+    { color: this.getColor(), url: '../Arquivos', name: 'Arquivos', roles: ['MASTER'], icon: 'home_storage' },
+    { color: this.getColor(), url: '../Cadastros', name: 'Cadastros', roles: ['MASTER'], icon: 'how_to_reg' },
+    { color: this.getColor(), url: '../Codex', name: 'Codex', roles: ['MASTER'], icon: 'code' },
+    { color: this.getColor(), url: '../DevTools', name: 'DevTools', roles: ['MASTER'], icon: 'handyman' },
+    { color: this.getColor(), url: '../Dynamic', name: 'Dynamic', roles: ['MASTER'], icon: 'dynamic_form' },
+    { color: this.getColor(), url: '../Financeiro', name: 'Financeiro', roles: ['MASTER'], icon: 'finance' },
+    { color: this.getColor(), url: '../Formularios', name: 'Formularios', roles: ['MASTER'], icon: 'fact_check' },
+    { color: this.getColor(), url: '../Gerencial', name: 'Gerencial', roles: ['MASTER'], icon: 'bookmark_manager' },
+    { color: this.getColor(), url: '../Icons', name: 'Icons', roles: ['MASTER'], icon: 'add_reaction' },
+    { color: this.getColor(), url: '../Imersao', name: 'Imersao', roles: ['MASTER'], icon: 'view_in_ar' },
+    { color: this.getColor(), url: '../Infra', name: 'Infra', roles: ['MASTER'], icon: 'lan' },
+    { color: this.getColor(), url: '../Instalacao', name: 'Instalacao', roles: ['MASTER'], icon: 'deployed_code' },
+    { color: this.getColor(), url: '../LowCode', name: 'LowCode', roles: ['MASTER'], icon: 'variable_add' },
+    { color: this.getColor(), url: '../Mensagens', name: 'Mensagens', roles: ['MASTER'], icon: 'chat' },
+    { color: this.getColor(), url: '../Organizacao', name: 'Organizacao', roles: ['MASTER'], icon: 'corporate_fare' },
+    { color: this.getColor(), url: '../Produtos', name: 'Produtos', roles: ['MASTER'], icon: 'inventory' },
+    { color: this.getColor(), url: '../Profile', name: 'Profile', roles: ['MASTER'], icon: 'face' },
+    { color: this.getColor(), url: '../Projetos', name: 'Projetos', roles: ['MASTER'], icon: 'tactic' },
+    { color: this.getColor(), url: '../SEO', name: 'SEO', roles: ['MASTER'], icon: 'robot_2' },
+    { color: this.getColor(), url: '../Threejs', name: 'Threejs', roles: ['MASTER'], icon: '3d_rotation' },
+    { color: this.getColor(), url: '../Treinamento', name: 'Treinamento', roles: ['MASTER'], icon: 'school' },
+    { color: this.getColor(), url: '../Vendas', name: 'Vendas', roles: ['MASTER'], icon: 'store' },
   ]
+
+  getColor() {
+    const hex = () => (13 + Math.floor(Math.random() * 3)).toString(16);
+    return `#${hex()}${hex()}${hex()}${hex()}${hex()}${hex()}`.toUpperCase()
+  }
 }
