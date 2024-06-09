055f006a97f5899078049afdd8718c6512d9285a
pLinho <paulorettamozo@gmail.com>
2024-06-09 01:08:34 -0300

    adicionado aba em files explorer;

--- apps/projects/ci-apps/arquivos/src/lib/ci-application/files/files.component.ts
+++ apps/projects/ci-apps/arquivos/src/lib/ci-application/files/files.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CoreModule } from '@ci/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'ci-files',
   standalone: true,
-  imports: [],
+  imports: [
+    CoreModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './files.component.html',
   styleUrl: './files.component.scss'
 })
