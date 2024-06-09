3bcb7a68bbdb228d6f8cd88939e6facf30fbb5fd
pLinho <paulorettamozo@gmail.com>
2024-06-09 00:36:56 -0300

    +++

--- apps/projects/ci-apps/arquivos/src/lib/ci-application/home/home.component.ts
+++ apps/projects/ci-apps/arquivos/src/lib/ci-application/home/home.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'ci-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
