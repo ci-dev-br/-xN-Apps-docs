4fe97e464b01ecd78e736180b69fdb9334b97994
pLinho <paulorettamozo@gmail.com>
2024-06-09 00:59:34 -0300

    created files explorer component;

---
+++ apps/projects/ci-apps/arquivos/src/lib/ci-application/files/files.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ci-files',
+  standalone: true,
+  imports: [],
+  templateUrl: './files.component.html',
+  styleUrl: './files.component.scss'
+})
+export class FilesComponent {
+
+}
--- apps/projects/ci-apps/arquivos/src/lib/ci-application/home/home.component.ts
+++ apps/projects/ci-apps/arquivos/src/lib/ci-application/home/home.component.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ci-home',
@@ -14,6 +15,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    RouterModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
