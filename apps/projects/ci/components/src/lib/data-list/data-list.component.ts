e0bdb2a9646500b699541d581575ae33412e69e3
pLinho <paulorettamozo@gmail.com>
2024-06-15 18:26:38 -0300

    #76: gerado arquivos do componente de data-list;

---
+++ apps/projects/ci/components/src/lib/data-list/data-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ci-data-list',
+  templateUrl: './data-list.component.html',
+  styleUrl: './data-list.component.scss'
+})
+export class DataListComponent {
+
+}
--- apps/projects/ci/components/src/lib/data-list/data-list.module.ts
+++ apps/projects/ci/components/src/lib/data-list/data-list.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataListComponent } from './data-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DataListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    DataListComponent
   ]
 })
 export class DataListModule { }
