d6576d94653349c518e2045713d5412afba0891f
pLinho <paulorettamozo@gmail.com>
2024-07-07 20:07:28 -0300

    Gerada biblioteca de `@ci/payment` e movido arquivos de `src/payment`;

--- server/src/payment/model/payment-authorization.entity.ts
+++
@@ -1,9 +0,0 @@
-import { FullAuditedEntity } from "@ci/core";
-import { Entity } from "typeorm";
-
-@Entity({
-    schema: 'payment'
-})
-export class PaymentAuthorization extends FullAuditedEntity {
-
-}
--- server/src/payment/model/payment-method.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, PrimaryColumn } from "typeorm";
-
-@Entity()
-export class MethodPayment {
-    @PrimaryColumn('character varying', { length: 18 })
-    invernalCode?: string;
-}
