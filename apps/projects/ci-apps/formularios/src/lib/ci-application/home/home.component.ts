dc44936a8c714acb96ae878db938e32a3f7e1a8c
pLinho <paulorettamozo@gmail.com>
2024-09-23 21:48:34 -0300

    Adicionado menu de ações em Principal de Formulários;

--- apps/projects/ci-apps/formularios/src/lib/ci-application/home/home.component.ts
+++ apps/projects/ci-apps/formularios/src/lib/ci-application/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { FileComponent } from '@ci/components';
 import { CoreModule } from '@ci/core';
 import { Form, FormsService } from '@ci/portal-api';
@@ -11,6 +14,9 @@ import { Form, FormsService } from '@ci/portal-api';
     CoreModule,
     MatToolbarModule,
     FileComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
