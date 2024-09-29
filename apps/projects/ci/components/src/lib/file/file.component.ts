84dc685452ef3607bae53ad054bc95aeff7c7067
pLinho <paulorettamozo@gmail.com>
2024-09-28 23:50:42 -0300

    Implementação de miniatura com iframe como ícone de componente de File;

--- apps/projects/ci/components/src/lib/file/file.component.ts
+++ apps/projects/ci/components/src/lib/file/file.component.ts
@@ -2,11 +2,21 @@ import { Component, Input } from "@angular/core";
 import { CoreModule } from "@ci/core";
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+
+
 @Component({
+    standalone: true,
+    imports: [
+        CoreModule,
+        MatCardModule,
+        MatIconModule,
+    ],
     selector: 'ci-file',
     template: `
         <mat-card>
-            @if(image){<img mat-card-image [src]="image" >}
+            @if(image){<img mat-card-image [src]="image" />}
+            @if(view){<iframe mat-card-image [src]="view | safe" ></iframe>}
             @if(title){{{title}}}
             @if(icon){<mat-icon>{{icon}}</mat-icon>}
         </mat-card>
@@ -20,18 +30,27 @@ import { MatIconModule } from "@angular/material/icon";
             flex:auto;
             overflow: hidden;
         }
+        iframe{
+            // overflow: hidden;
+            border: none;
+            transform: scale(.25);
+            width: 400%;
+            height: 400%;
+            margin: -150%;
+            pointer-events: none;
+            user-select: none;
+        }
         `
     ],
-    standalone: true,
-    imports: [
-        CoreModule,
-        MatCardModule,
-        MatIconModule,
-    ]
+
 })
 export class FileComponent {
     @Input() icon?: string;
     @Input() image?: string;
     @Input() title?: string;
+    @Input() view?: string | undefined;
     @Input() description?: string;
+    constructor(
+        private readonly s: DomSanitizer
+    ) { }
 }
