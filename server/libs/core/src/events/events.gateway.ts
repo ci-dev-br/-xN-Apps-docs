36f00decb0dc1be021ca5c9016d14ca9ebfdbd63
pLinho <paulorettamozo@gmail.com>
2024-10-30 22:41:10 -0300

    +++

--- server/libs/core/src/events/events.gateway.ts
+++ server/libs/core/src/events/events.gateway.ts
@@ -2,9 +2,13 @@ import { DomainService } from "@ci/manager";
 import { ConnectedSocket, MessageBody, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { createHash } from "crypto";
 import { Server } from "ws";
+
 @WebSocketGateway({
-    transports: ['websocket'],
-    cors: [DomainService.whitelist, // TODO: não pode ser utilizado cliente coringa. Deve ser criada modelo de Domínios permitidos, cada um com suas blacklists de bloqueio se ouver e regras adicionais de política de acesso pode ser necessárias.
+    transports: [
+        'websocket'
+    ],
+    cors: [DomainService.whitelist,     
+        // TODO: não pode ser utilizado cliente coringa. Deve ser criada modelo de Domínios permitidos, cada um com suas blacklists de bloqueio se ouver e regras adicionais de política de acesso pode ser necessárias.
     ],
 })
 export class EventsGateway implements OnGatewayInit {
