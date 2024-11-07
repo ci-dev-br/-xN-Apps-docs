c0a0d16d5f6649560e84dd3d00d75b31f8f20f80
pLinho <paulorettamozo@gmail.com>
2024-11-06 23:35:56 -0300

    Pesquisar usuário ou pessoa para contato

--- apps/projects/ci-apps/mensagens/src/lib/pesquisar-contato/pesquisar-contato.component.ts
+++ apps/projects/ci-apps/mensagens/src/lib/pesquisar-contato/pesquisar-contato.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
@@ -8,6 +10,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     MatToolbarModule,
     MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   templateUrl: './pesquisar-contato.component.html',
   styleUrl: './pesquisar-contato.component.scss'
