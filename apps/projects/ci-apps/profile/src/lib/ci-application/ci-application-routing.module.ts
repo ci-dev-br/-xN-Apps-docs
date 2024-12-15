f9718532a3cfd4c5c71529889ceca634a9c82cb5
pLinho <paulorettamozo@gmail.com>
2024-12-15 16:51:01 -0300

    Adicionado notas em tenant; + notas em seo-marketing;

--- apps/projects/ci-apps/profile/src/lib/ci-application/ci-application-routing.module.ts
+++ apps/projects/ci-apps/profile/src/lib/ci-application/ci-application-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: {
-    group: 'UserMenu',
-  } }
+  {
+    path: '', component: HomeComponent, data: {
+      group: 'user-menu',
+    }
+  }
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- server/libs/seo-marketing/src/models/back-link.entity.ts
+++ server/libs/seo-marketing/src/models/back-link.entity.ts
@@ -1,7 +1,11 @@
 import { FullAuditedEntity } from "@ci/manager";
-import { Entity } from "typeorm";
+import { Column, Entity } from "typeorm";
 
+/**
+ * Marketing - CRM - Link de Retorno - registra o link de rertorno e um link recebido dentro do sistema. O link de retorno permite registrar um retorno para disparo em determinadas automações.
+ */
 @Entity({ schema: 'marketing' })
 export class BackLink extends FullAuditedEntity {
-    
+    @Column({ nullable: true })
+    enderecoRetorno?: string;
 }
