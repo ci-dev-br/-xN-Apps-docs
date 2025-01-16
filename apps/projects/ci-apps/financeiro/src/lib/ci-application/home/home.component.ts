f8a80592953afef3340392c4148290e2fefe5d66
pLinho <paulorettamozo@gmail.com>
2025-01-16 13:11:34 -0300

    ajustes na home de app Financeiro

--- apps/projects/ci-apps/financeiro/src/lib/ci-application/home/home.component.ts
+++ apps/projects/ci-apps/financeiro/src/lib/ci-application/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'ci-home',
-    imports: [],
+    standalone: true,
+    imports: [
+        MatIconModule,
+        MatButtonModule,
+    ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
