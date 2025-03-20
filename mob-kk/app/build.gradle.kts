c11000643a94077b2901df8de18b61a50daa9395
pLinho <paulorettamozo@gmail.com>
2025-03-20 18:40:05 -0300

    android clients

--- mob-kk/app/build.gradle.kts
+++ mob-kk/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "br.dev.ci.mobmanger_kitkat"
-    compileSdk = 35
+    compileSdk = 19
 
     defaultConfig {
         applicationId = "br.dev.ci.mobmanger_kitkat"
@@ -33,10 +33,8 @@ android {
 
 dependencies {
 
-    implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
-    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
