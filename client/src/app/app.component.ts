a261eeba5f3f9cd8805508b864f7b34aaf2fc808
pLinho <paulorettamozo@gmail.com>
2025-03-24 05:37:54 -0300

    ...

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, Optional, isDevMode } from '@angular/core';
-import { ChildActivationStart, NavigationError, Router } from '@angular/router';
+import { NavigationError, Router } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
-import { map } from 'rxjs';
 import { UserService } from './services/user.service';
 import { PaginaErroComponent } from './views/pagina-erro/pagina-erro.component';
 import { LocalizationService } from './core/services/localization.service';
