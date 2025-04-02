2210272a8f58200c426ffe2667080e386ae7de3f
pLinho <paulorettamozo@gmail.com>
2025-04-02 02:40:22 -0300

    Adicionado demais implementeções da camada de comunicação com aplicação servidora;

--- MobManager-kk/app/src/main/java/br/dev/ci/mobmanager/MainActivity.java
+++ MobManager-kk/app/src/main/java/br/dev/ci/mobmanager/MainActivity.java
@@ -1,15 +1,34 @@
 package br.dev.ci.mobmanager;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import br.dev.ci.mobmanager.client.ManagerClient;
+import br.dev.ci.mobmanager.client.model.PhoneNumber;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_READ_PHONE_STATE = 1;
+    private List<PhoneNumber> phones;
+    private TextView message;
+    private Button appsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +39,66 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        String api = "https://srv33.internals.ci.dev.br:664/";
+        String ws = "wss://srv33.internals.ci.dev.br:664/";
+        if(this.message != null){
+            this.message.setText("Iniciando conexção... (1)");
+            permission();
+            getPhoneNumber();
+            this.message.setText("Identificando números disponíveis");
+            adicionarItem(api, ws);
+            this.message.setText("Dipositivo identificado");
+        }
+        if(this.appsButton != null){
+            this.appsButton.setOnClickListener(v -> this.openApps());
+        }
+    }
+    private void openApps(){
+
+
+        /*AlertDialog.Builder builder = new  AlertDialog.Builder(this);
+        builder
+                .setTitle("Apps")
+                .setView(View.inflate(this,R.layout.list_apps_fragment_item_list,null))
+        ;
+        AlertDialog dialog = builder.create();
+
+        dialog.show();*/
+    }
+    public void adicionarItem(String api, String ws) {
+        ManagerClient.getInstance().addGateway(api, ws);
+    }
+    private void permission() {
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+        }
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, REQUEST_READ_PHONE_STATE);
+        }
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_NUMBERS}, REQUEST_READ_PHONE_STATE);
+        }
+    }
+    private void getPhoneNumber() {
+        if (/*ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED && */ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_PHONE_NUMBERS) == PackageManager.PERMISSION_GRANTED /*&& ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED*/ || true) {
+
+            /*SubscriptionManager subscriptionManager = (SubscriptionManager) getApplicationContext().getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+            List<SubscriptionInfo> subscriptionInfoList = subscriptionManager.getActiveSubscriptionInfoList();
+            this.phones = new ArrayList<>();
+            for (SubscriptionInfo subscriptionInfo : subscriptionInfoList) {
+                int subscriptionId = subscriptionInfo.getSubscriptionId();
+                String carrierName = subscriptionInfo.getCarrierName().toString();
+                String number = subscriptionInfo.getNumber();
+
+                // TelephonyManager telephonyManager = ((TelephonyManager) requireContext().getSystemService(Context.TELEPHONY_SERVICE)).createForSubscriptionId(subscriptionId);
+                PhoneNumber phone_number = new PhoneNumber(){{
+                    setNumber(number);
+                    setCarrierName(carrierName);
+                    setSubscriptionId(subscriptionId);
+                }}; //  telephonyManager.getLine1Number();
+                this.phones.add(phone_number);
+            }*/
+            ManagerClient.getInstance().setPhones(this.phones);
+        }
     }
 }
--- MobManager-kk/app/src/main/java/br/dev/ci/mobmanager/client/DeviceConnect.java
+++ MobManager-kk/app/src/main/java/br/dev/ci/mobmanager/client/DeviceConnect.java
@@ -2,6 +2,8 @@ package br.dev.ci.mobmanager.client;
 
 import android.os.AsyncTask;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -11,7 +13,18 @@ import java.net.URL;
 import br.dev.ci.mobmanager.client.model.Device;
 import br.dev.ci.mobmanager.client.model.GatewayConnection;
 
+/**
+ * Device Connect - Gerenciador de Conexão com a Aplicação Servidora
+ *
+ * Conecte o aplicativo com a aplicação Gateway e intercale os
+ * serviços entre smartphone e servidor
+ */
 public class DeviceConnect extends AsyncTask<Device, Void, String> {
+    /**
+     *   Gson gson = new Gson();
+     *   Pessoa pessoa = gson.fromJson(jsonString, Pessoa.class);
+     */
+    private Gson mapper = new Gson();
     private GatewayConnection url_gateway;
     public DeviceConnect(GatewayConnection url_gateway) {
         this.url_gateway = url_gateway;
@@ -30,7 +43,7 @@ public class DeviceConnect extends AsyncTask<Device, Void, String> {
             connection.setRequestProperty("Accept", "application/json");
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
 
-            // outputStreamWriter.write(mapper.writeValueAsString(devices[0]));
+            outputStreamWriter.write(mapper.toJson(devices[0]));
             outputStreamWriter.flush();
 
             try (BufferedReader br = new BufferedReader(
