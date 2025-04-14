0c59324b2e48049523e2615618e4914f5e2143de
pLinho <paulorettamozo@gmail.com>
2025-04-14 11:22:34 -0300

    Added mac on payload device connect

--- apps/projects/ci/portal-api/src/lib/models/device-payload.ts
+++ apps/projects/ci/portal-api/src/lib/models/device-payload.ts
@@ -7,6 +7,7 @@ export interface DevicePayload {
   applicationId?: string | null;
   authentication?: string | null;
   id?: string | null;
+  mac?: string | null;
   model?: string | null;
   name?: string | null;
   numbers?: Array<PhoneNumber> | null;
