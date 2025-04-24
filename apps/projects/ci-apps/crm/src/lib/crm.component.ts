90154a359baa4747c75c12d2ecacf8153a3f4936
pLinho <paulorettamozo@gmail.com>
2025-04-24 13:49:55 -0300

    Update app CPR

--- apps/projects/ci-apps/crm/src/lib/crm.component.ts
+++ apps/projects/ci-apps/crm/src/lib/crm.component.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ci-crm',
+  standalone: true,
   imports: [],
-  template: `
-    <p>
-      crm works!
-    </p>
-  `,
+  template: ` `,
   styles: ``
 })
 export class CrmComponent {
