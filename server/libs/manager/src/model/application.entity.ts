4a659def0fd2a747c016e46bfdc548a92c36f28a
pLinho <paulorettamozo@gmail.com>
2025-05-04 02:04:51 -0300

    Adicionado infomrações de titulo no dto de Application

--- server/libs/manager/src/model/application.entity.ts
+++ server/libs/manager/src/model/application.entity.ts
@@ -4,31 +4,34 @@ import { Domain } from "./domain.entity";
 import { schema } from "../noms";
 @Entity({ schema })
 export class Application {
-    @ApiProperty({ nullable: true, required: false, uniqueItems: true })
+    @ApiProperty({ nullable: true, required: false, uniqueItems: true, title: 'Chave Interna', readOnly: true })
     @PrimaryGeneratedColumn('uuid')
     id?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Endereço URL' })
     @Column({ nullable: true })
     url?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Ícone' })
     @Column({ nullable: true })
     icon?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({
+        title: 'Nome',
+        nullable: true, required: false
+    })
     @Column({ nullable: true })
     name?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Descrição' })
     @Column({ nullable: true })
     description?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Papéis' })
     @Column({ nullable: true, type: 'varchar', array: true })
     roles?: string[];
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Domínio' })
     @ManyToMany(() => Domain)
     domain?: Domain;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Grupo' })
     @Column({ default: 'global', length: 13 })
     menuGroupName: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Categoria' })
     @Column({ length: 30, nullable: true })
     categoria: string;
     @ApiProperty({
