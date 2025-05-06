2cf348919433f3a5aeb1c6ce63dee0dc2eeaf2aa
pLinho <paulorettamozo@gmail.com>
2025-05-05 23:40:50 -0300

    Adicionado Board em início de Apps/Gerencial;

--- apps/projects/ci-apps/gerencial/src/lib/ci-application/home/home.component.ts
+++ apps/projects/ci-apps/gerencial/src/lib/ci-application/home/home.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { BoardModule } from '@ci/components';
 
 @Component({
     selector: 'ci-home',
+    standalone: true,
     imports: [
         MatButtonModule,
         MatIconModule,
         RouterModule,
+        BoardModule,
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
