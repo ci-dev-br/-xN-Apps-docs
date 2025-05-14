a22fe2411091c32d8fe03fc6dc65c996904a9dcb
pLinho <paulorettamozo@gmail.com>
2025-05-13 23:07:43 -0300

    Implementação da chamada de editar; Implementação da chamada de novo;

---
+++ apps/projects/ci-apps/cadastros/src/lib/cadastros/master-detail/editar/editar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { CoreModule } from "@ci/core";
+@Component({
+    selector: 'ci-master-datail--editar',
+    template: ``,
+    imports: [
+        CoreModule,
+    ],
+    standalone: true,
+})
+export class EditarComponent {
+    constructor() { }
+
+}
--- apps/projects/ci-apps/cadastros/src/lib/cadastros/master-detail/master-detail.component.ts
+++ apps/projects/ci-apps/cadastros/src/lib/cadastros/master-detail/master-detail.component.ts
@@ -1,8 +1,13 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { ActivatedRoute, RouterModule } from "@angular/router";
-import { GridModule, IDataGridOptions } from "@ci/components";
+import { GridModule, IDataGridOptions, WindowModule, WindowService } from "@ci/components";
 import { CoreModule, DaoBuilder } from "@ci/core";
 import { Application } from "@ci/portal-api";
+import { EditarComponent } from "./editar/editar.component";
 
 @Component({
     selector: 'ci-master-detail',
@@ -11,23 +16,42 @@ import { Application } from "@ci/portal-api";
         CoreModule,
         GridModule,
         RouterModule,
+        MatToolbarModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatButtonModule,
+        WindowModule,
     ],
     template: `
-    {{schemaName || ''}}
+    <mat-toolbar [auto-scroll]="'horizontal'">
+         {{schemaName || ''}}
+    <button mat-raised-button (click)="createNew()" >
+        Novo
+    </button>
+    <span style="flex:auto"></span>
+    <mat-button-toggle-group >
+        <mat-button-toggle value="table"><mat-icon>view_list</mat-icon>Tabela</mat-button-toggle>
+        <mat-button-toggle value="list"><mat-icon>grid_view</mat-icon>Lista</mat-button-toggle>
+    </mat-button-toggle-group>
+</mat-toolbar>
+   
     <!-- TODO: Visualização em lista e em tabela permitindo visualização lateral ou em janela dos valores selecionados. -->
     <ci-data-grid [options]="gridOptions" [source]="source"></ci-data-grid>
     `
 })
 export class MasterDetailComponent<T> implements OnInit {
+    visualizacao: 'table' | 'list' = 'table';
+
     @Input()
     schemaName?: string;
     gridOptions?: IDataGridOptions<Application>;
     constructor(
         private readonly daoBuilder: DaoBuilder,
         private readonly route: ActivatedRoute,
+        private readonly window: WindowService,
     ) {
     }
-    source?: T[] = [];
+    source?: T[] = [{} as any];
     async loadGrid() {
         if (!!this.schemaName) {
             const properties = await (await this.daoBuilder.getSchema(this.schemaName)).properties
@@ -50,5 +74,14 @@ export class MasterDetailComponent<T> implements OnInit {
             this.schemaName = params.get('EntityName') || undefined;
             this.loadGrid();
         });
+    }
+    async editar(application: Application) {
+        return await this.window.open(EditarComponent, application)
+    }
+    async createNew() {
+        let new_instance = {};
+        const data = await this.editar(new_instance);
+        if (!!data?.internalId || !!data?.id) // TODO: revisar esta regra
+            this.source = [data, ...this.source || []];
     }
 }
