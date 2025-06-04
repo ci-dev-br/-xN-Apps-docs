9147f5da17e2998538087313a8aef376bd40f6f9
pLinho <paulorettamozo@gmail.com>
2025-06-03 22:21:41 -0300

    Adicionado intefaces de serviços de entidades padrão

--- apps/projects/ci/core/src/public-api.ts
+++ apps/projects/ci/core/src/public-api.ts
@@ -10,3 +10,4 @@ export * from './lib/provider';
 export * from './lib/io/http.interceptor';
 export * from './lib/io/ws.service';
 export * from './lib/decorators/short-cut.decorator';
+export * from './lib/dao/i-have-sync';
