8cd79ac0f1caaeb383c0e641345b66344194bd89
pLinho <paulorettamozo@gmail.com>
2025-06-09 04:11:03 -0300

    Add title on model Website

--- server/libs/cms/src/models/website.entity.ts
+++ server/libs/cms/src/models/website.entity.ts
@@ -10,22 +10,22 @@ export class Website extends FullAuditedEntity {
     @ApiProperty({ nullable: true, required: false })
     @Column({ type: 'jsonb' })
     atributes: { [atributeCodename: string]: string };
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Nome' })
     @Column({ nullable: true })
     name?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Domínio' })
     @Column({ nullable: true })
     domain?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, title: 'Theme' })
     @Column({ nullable: true })
     theme?: string;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, isArray: true, title: 'Módulos' })
     @Column({ type: 'varchar', array: true })
     modules?: string[];
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, type: User, title: 'Administrador' })
     @ManyToMany(type => User)
     admin?: User;
-    @ApiProperty({ nullable: true, required: false })
+    @ApiProperty({ nullable: true, required: false, type: User, isArray: true, title: 'Usuários Autorizados' })
     @ManyToMany(type => User)
     users?: User[];
 }
