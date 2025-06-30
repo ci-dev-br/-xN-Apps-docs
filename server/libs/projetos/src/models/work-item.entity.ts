4ae4879ae80ed206810db1c10e7c3e1d334dd09a
pLinho <paulorettamozo@gmail.com>
2025-06-29 21:16:57 -0300

    Ajustes de Works

--- server/libs/projetos/src/models/work-item.entity.ts
+++ server/libs/projetos/src/models/work-item.entity.ts
@@ -1,11 +1,17 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany } from "typeorm";
 import { schema } from "./schema";
 import { FullAuditedEntity } from "@ci/manager";
 import { ApiProperty } from "@nestjs/swagger";
+import { Projeto } from "./projeto.entity";
 @Entity({
     schema,
 })
 export class WorkItem extends FullAuditedEntity {
-    @ApiProperty({ nullable: true, required: false }) @Column({ nullable: true })
+    @ApiProperty({ nullable: true, required: false })
+    @Column({ nullable: true })
     code?: number;
+    @ApiProperty({ type: Projeto })
+    @ManyToMany(() => Projeto)
+    @JoinTable()
+    project?: Projeto;
 }
