c8d866c5f1483fac8477f3b762e8a3ea971ce7e3
pLinho <paulorettamozo@gmail.com>
2025-07-08 13:40:26 -0300

    adicionado toolbar e footer em termos

--- apps/src/app/termos/termos.component.ts
+++ apps/src/app/termos/termos.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
     selector: 'ci-termos',
-    imports: [],
+    standalone: true,
+    imports: [
+        MatToolbarModule,
+    ],
     templateUrl: './termos.component.html',
     styleUrl: './termos.component.scss'
 })
