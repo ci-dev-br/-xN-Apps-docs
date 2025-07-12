b44ad4cc306f799a41a6ffe4f4a8c5cbe7dd3584
pLinho <paulorettamozo@gmail.com>
2025-07-12 15:53:12 -0300

    adicionado ttimeout em chamada da aplicação para 1 segundo, alterando para arquivo em cache em caso de falha.

--- apps/public/index.php
+++ apps/public/index.php
@@ -1,4 +1,16 @@
 <?php 
-$jsonData = file_get_contents('https://apps.ci.dev.br'.$_SERVER['REQUEST_URI'].'?from='.$_SERVER['HTTP_HOST']);
-print $jsonData;
-// include 'index.csr.html';
+$ctx = stream_context_create(array('http'=>
+    array(
+        'timeout' => 1,  //1200 Seconds is 20 Minutes
+    )
+));
+
+try {
+    //code...
+    $jsonData = file_get_contents('https://apps.ci.dev.br'.$_SERVER['REQUEST_URI'].'?from='.$_SERVER['HTTP_HOST'], false, $ctx);
+    print $jsonData;
+} catch (\Throwable $th) {
+    //throw $th;
+}
+
+include 'index.csr.html';
