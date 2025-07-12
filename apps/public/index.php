2c05ed7a27a486e4d54e57917230df9a5ce43129
pLinho <paulorettamozo@gmail.com>
2025-07-12 16:40:39 -0300

    ajuste em retorno do content-type da origem

--- apps/public/index.php
+++ apps/public/index.php
@@ -1,16 +1,13 @@
 <?php 
-$ctx = stream_context_create(array('http'=>
-    array(
-        'timeout' => 1,  //1200 Seconds is 20 Minutes
-    )
-));
-
 try {
-    //code...
-    $jsonData = file_get_contents('https://apps.ci.dev.br'.$_SERVER['REQUEST_URI'].'?from='.$_SERVER['HTTP_HOST'], false, $ctx);
-    print $jsonData;
+    $contents = file_get_contents('https://apps.ci.dev.br'.$_SERVER['REQUEST_URI'].'?from='.$_SERVER['HTTP_HOST']);
+    $headers = implode("\n", $http_response_header);
+    if (preg_match_all("/^content-type\s*:\s*(.*)$/mi", $headers, $matches)) {
+        $content_type = end($matches[1]);
+        header("ContentType: $content_type");
+    }
+    print($contents);
 } catch (\Throwable $th) {
     //throw $th;
+    include 'index.csr.html';
 }
-
-include 'index.csr.html';
