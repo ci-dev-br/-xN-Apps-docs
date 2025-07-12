5b7fa461ec959e4bd711795cf5e1e22dce86fa77
pLinho <paulorettamozo@gmail.com>
2025-07-12 15:49:45 -0300

    correção em escrita

--- apps/projects/ci/core/src/lib/routes/route-from-schema.ts
+++ apps/projects/ci/core/src/lib/routes/route-from-schema.ts
@@ -2,7 +2,7 @@ import { Type } from "@angular/core";
 import { Route } from "@angular/router";
 import { MasterDetailComponent } from "@ci/components";
 
-export function RouteFromSchema(c: string, componentBase: Type<any> = MasterDetailComponent;) {
+export function RouteFromSchema(c: string, componentBase: Type<any> = MasterDetailComponent) {
     return {
         path: `${c}`, component: componentBase, data: {
             schema: `${c}`, title: `${c}`, icon: `svg:${c}`,
