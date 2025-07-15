2491044635e990d2b607403226e882adacf4e611
pLinho <paulorettamozo@gmail.com>
2025-07-15 00:48:10 -0300

    Ajuste em redirecionamento PHP, cloudflare acaba bloqueando após muitos acessos. Talvez falte cabeçalho para esse tipo de requisição;

--- apps/public/index.php
+++ apps/public/index.php
@@ -1,19 +1,24 @@
 <?php 
-try {
-    $opts = [
-        "http" => [
-            "header" => "x-From: ".$_SERVER['HTTP_HOST']."\r\n",
-            'timeout' => 2
-        ]
-    ];
-    $context = stream_context_create($opts);
-    $contents = file_get_contents('https://apps.ci.dev.br'.$_SERVER['REQUEST_URI'],false, $context);
-    $headers = implode("\n", $http_response_header);
-    if (preg_match_all("/^content-type\s*:\s*(.*)$/mi", $headers, $matches)) {
-        $content_type = end($matches[1]);
-        header("Content-Type: $content_type");
-    }
-    print($contents);
-} catch (\Throwable $th) {
-    include 'index.csr.html';
-}
+// $step = 0;
+// try {
+//     $opts = [
+//         "http" => [
+//             "header" => "x-From: ".$_SERVER['HTTP_HOST']."\r\n",
+//             'timeout' => 4
+//         ]
+//     ];
+//     $context = stream_context_create($opts);
+//     $contents = file_get_contents('https://apps.ci.dev.br'.$_SERVER['REQUEST_URI'],false, $context);
+//     $headers = implode("\n", $http_response_header);
+//     if (preg_match_all("/^content-type\s*:\s*(.*)$/mi", $headers, $matches)) {
+//         $content_type = end($matches[1]);
+//         header("Content-Type: $content_type");
+//     }
+//     print($contents);
+//     $step = 1;
+// } catch (\Throwable $th) {
+// }
+// if($step == 0){
+//     include 'index.csr.html';
+// }
+include 'index.csr.html';
