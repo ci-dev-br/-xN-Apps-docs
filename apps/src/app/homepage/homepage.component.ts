cc11a5796da871b0b079b587c2804520eb0785cc
pLinho <paulorettamozo@gmail.com>
2025-10-25 23:43:31 -0300

    Nova implementação em sidebar da homepage do usuário

--- apps/src/app/homepage/homepage.component.ts
+++ apps/src/app/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ComponentRef, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ComponentRef, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Router, RouterModule } from '@angular/router';
 import { AuthModule, UserService } from '@ci/auth';
@@ -43,7 +43,10 @@ export class HomepageComponent implements OnInit {
         private el: ElementRef<Element>,
         private readonly router: Router,
         private readonly dialog: MatDialog,
-    ) { }
+    ) {
+
+    }
+    protected sidebar = false;
     protected categorias?: any[];
     protected apps?: Application[];
     protected bgs = [
@@ -55,11 +58,49 @@ export class HomepageComponent implements OnInit {
         this.userService.user.subscribe(user => this.updateUser(user));
         // Set the playback speed to 0.5 (half speed)
         if (this.video?.nativeElement) this.video.nativeElement.playbackRate = 0.1;
+
+        if (!!document && !!document.body && !!window) {
+            this.animacao();
+        }
     }
+
+    async animacao() {
+        // TODO: animação entre 15 a 25 px na horizontal e vertical movendo lentamente de forma aleatória dentro desse limite 
+        let limiteTop = 5;
+        let limiteBottom = 35;
+        let limiteLeft = 5;
+        let limiteRight = 35;
+        let directionY = 1;
+        let directionX = 1;
+        let posY = 0;
+        let posX = 0;
+        let velocidade = 0.15;
+        let anima = () => {
+            if (posY >= limiteBottom) directionY = -1;
+            if (posY <= limiteTop) directionY = 1;
+            if (posX >= limiteRight) directionX = -1;
+            if (posX <= limiteLeft) directionX = 1;
+            posY += directionY * 4 * Math.random();
+            posX += directionX * 4 * Math.random();
+
+            if (this.sidebarEl?.nativeElement.classList.contains('--inactive')) {
+                this.sidebarTop = (45 * Math.random()) + posY;
+                this.sidebarLeft = (45 * Math.random()) + posX;
+            } else {
+                this.sidebarTop = 5;
+                this.sidebarLeft = 5;
+            }
+            setTimeout(() => anima(), 900);
+        }
+        anima();
+    }
+
     protected bg?: string;
     async mountStyle() {
         this.bg = this.bgs[Math.round((this.bgs.length - 1) * Math.random())];
     }
+
+    @ViewChild('sidebarElement') protected sidebarEl?: ElementRef<HTMLElement>;
     private updateUser(user: User | null) {
         //
         this.apps = [
@@ -80,5 +121,19 @@ export class HomepageComponent implements OnInit {
                 origin: this
             }
         });
+    }
+    sidebarTop = 25;
+    sidebarLeft = 25;
+    @HostListener('document:mousemove', ['$event'])
+    protected isOutsideSidebar(event: MouseEvent) {
+        let limit = 124;
+        if (
+            (this.sidebarEl && this.sidebarEl?.nativeElement.getBoundingClientRect().bottom + limit < event.clientY) ||
+            (this.sidebarEl && this.sidebarEl?.nativeElement.getBoundingClientRect().top - limit > event.clientY) ||
+            (this.sidebarEl && this.sidebarEl?.nativeElement.getBoundingClientRect().right + limit < event.clientX) ||
+            (this.sidebarEl && this.sidebarEl?.nativeElement.getBoundingClientRect().left - limit > event.clientX)
+        ) {
+            this.sidebar = false;
+        }
     }
 }
