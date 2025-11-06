92b3561712ca88d37a21c42ca8d2498f82999b2c
pLinho <paulorettamozo@gmail.com>
2025-11-05 22:00:18 -0300

    Ajustes em board

--- apps/projects/ci/components/src/lib/settings/settings.component.ts
+++ apps/projects/ci/components/src/lib/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { WindowService } from "@ci/components";
 import { CoreModule } from "@ci/core";
 export interface ISettingOption {
@@ -12,10 +14,18 @@ export interface ISettings {
     selector: 'ci-settings',
     imports: [
         CoreModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
     template: ` 
-
-    `
+    <mat-form-field>
+        <input matInput type="text" placeholder="Painéis" />
+    </mat-form-field>
+    `, styles: `
+    :host{
+        display: flex;
+        flex-direction: column;
+    }`
 })
 export class SettingsComponent {
     // title = 'Configurações';
