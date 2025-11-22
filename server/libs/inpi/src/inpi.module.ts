3c845ee6a3c234fe3eb463913719335dfd66afbe
pLinho <paulorettamozo@gmail.com>
2025-11-22 15:18:06 -0300

    updaude inpi icon

--- server/libs/inpi/src/inpi.module.ts
+++ server/libs/inpi/src/inpi.module.ts
@@ -6,6 +6,7 @@ import { ClienteINPI } from "./model/cliente-inpi.entity";
 import { PrazoINPI } from "./model/prazo-inpi.entity";
 import { ProcessoINPI } from "./model/process-inpi.entity";
 import { AtivoIntelectual } from "./model/ativo-intelectual.entity";
+import { ManagerModule } from "@ci/manager";
 export const INPIEntities = [
     Marca,
     Patente,
@@ -18,7 +19,17 @@ export const INPIEntities = [
     imports: [
         TypeOrmModule.forFeature([
             ...INPIEntities
-        ])
+        ]),
+        ManagerModule.forApplication({
+            application: {
+                icon: '/icons/apps/inpi.svg',
+                title: 'INPI',
+                description: 'Marcas e Patentes',
+                rules: [
+                    'ADMIN'
+                ]
+            }
+        }),
     ],
 })
 export class INPIModule { }
