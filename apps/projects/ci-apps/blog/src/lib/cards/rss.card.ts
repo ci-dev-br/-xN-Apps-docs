ce1313b24ee9b198eab6861172e728b6e7e9f8d4
pLinho <paulorettamozo@gmail.com>
2025-12-02 22:23:43 -0300

    ajustes gerais

--- apps/projects/ci-apps/blog/src/lib/cards/rss.card.ts
+++ apps/projects/ci-apps/blog/src/lib/cards/rss.card.ts
@@ -1,22 +1,68 @@
-import { Component, Input } from "@angular/core";
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from "@angular/common/http";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { FormBuilder, FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { CoreModule } from "@ci/core";
+import { XMLParser, XMLBuilder, XMLValidator } from 'fast-xml-parser';
+import { lastValueFrom } from "rxjs";
 
 @Component({
     selector: 'ci-card--crm-totalizador',
     template: `
-        <h4>Total de Vendas</h4>
-        <p>
-            R$ 145,20
-        </p>
+        @if(stage==='config'){
+            <h4>Configure o serviço de RSS</h4>
+            <p>Configure o cartão de RSS inserindo o endereço do RSS abaixo:</p>
+            <mat-form-field>
+                <input matInput placeholder="URL" [(ngModel)]="url" (blur)="update()" />
+        </mat-form-field>}
+        @else{
+            @if(!!data?.title){
+                <h1>{{data?.title}}</h1>
+            }
+        }
     `,
     standalone: true,
     imports: [
         CoreModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
     ]
 })
-export class RSSCard {
-    @Input
-        () url?: string;
+export class RSSCard implements OnChanges {
+    stage: 'config' | 'run' = 'config';
+    data?: any;
+    @Input() url?: string;
+    protected async update() {
+        if (this.url && this.url?.trim().length > 0) {
+            await this.loadRSS();
+            this.stage = 'run';
+        }
+    }
+    constructor(
+        private readonly fb: FormBuilder,
+        private readonly http: HttpClient,
+    ) { }
+    ngOnChanges(changes: SimpleChanges): void {
+
+    }
+    async loadRSS() {
+        try {
+            if (!this.url) return;
+            const xml_parser = new XMLParser();
+            let json_rss_loaded = xml_parser.parse(
+                await lastValueFrom(this.http.get(this.url, {
+                    responseType: 'text'
+                })));
+
+            this.data = json_rss_loaded;
+            // const builder = new XMLBuilder();
+            // const xmlContent = builder.build(json_rss_loaded);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 export const RSSCardInfo = {
     title: 'RSS',
---
+++ server/libs/core/src/events/client-socket.ts
@@ -0,0 +1,6 @@
+export class ClienteSocket {
+    constructor() {
+
+    }
+
+}
