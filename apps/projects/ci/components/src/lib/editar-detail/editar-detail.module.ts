2b0c775cae25f88cf9e3c0a12803320dc2c4de9d
pLinho <paulorettamozo@gmail.com>
2025-12-06 16:23:12 -0300

    refatoração: ajustes na nomenclatura.

---
+++ apps/projects/ci/components/src/lib/editar-detail/editar-detail.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * Editar Detail Module
+ */
+@NgModule({
+  declarations: [
+
+  ],
+  exports: [
+
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class EditarDetailModule { }
