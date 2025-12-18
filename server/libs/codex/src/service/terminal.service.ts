76770b4753e6153ccceff2b9e15d21640b6a8eff
pLinho <paulorettamozo@gmail.com>
2025-12-18 13:34:04 -0300

    ....

--- server/libs/codex/src/service/terminal.service.ts
+++ server/libs/codex/src/service/terminal.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import * as os from 'os';
-import * as pty from 'node-pty';
+// import * as pty from 'node-pty';
 
 @Injectable()
 export class TerminalService {
   // Mapa para guardar a sessão de terminal de cada socket conectado
-  private sessions: Map<string, IPty> = new Map();
+private sessions: Map<string, any> = new Map();
   createSession(socketId: string, onData: (data: string) => void) {
     // Detecta o shell padrão do sistema (PowerShell no Win, Bash/Zsh no Linux/Mac)
     const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
     // Cria o processo do terminal
-    const ptyProcess = pty.spawn(shell, [], {
+    const ptyProcess = null; /* pty.spawn(shell, [], {
       name: 'xterm-color',
       cols: 80,
       rows: 30,
       cwd: process.env.HOME || process.cwd(),
       env: process.env as any,
-    });
+    });*/
     // Escuta a saída do terminal (o que o comando retorna) e envia para o callback
     ptyProcess.onData((data) => {
       onData(data);
