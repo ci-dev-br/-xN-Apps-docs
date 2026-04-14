477cd4285b282da36581070a035daff41d5b4d7c
pLinho <paulorettamozo@gmail.com>
2026-04-14 13:32:50 -0300

    refactor: Remove temporariamente botão "Criar novo Projeto de Ícone"
    Foi comentado o botão "Criar novo Projeto de Ícone" na tela de home (home.component.html) e seu respectivo import de `RouterModule` em (home.component.ts).
    
    Esta ação é temporária e visa remover um elemento que estava provisoriamente presente na interface, conforme indicado pelo `TODO` no próprio HTML (`<!-- TODO: isso não pode estar aqui -->`). A funcionalidade de criação de novo projeto de ícone será endereçada e implementada de forma mais adequada em um contexto futuro, visando uma melhor arquitetura de informação e experiência do usuário.
    
    Próxima ação: Avaliar e implementar a forma definitiva para iniciar a criação de um novo projeto de ícone, possivelmente em uma rota dedicada ou através de um fluxo de trabalho mais estruturado.

--- apps/projects/ci-apps/icons/src/lib/ci-application/home/home.component.ts
+++ apps/projects/ci-apps/icons/src/lib/ci-application/home/home.component.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
-import { RouterModule } from '@angular/router';
-
+// import { RouterModule } from '@angular/router';
 @Component({
     selector: 'ci-home',
     imports: [
         CommonModule,
         MatButtonModule,
-        RouterModule,
+        // RouterModule,
     ],
     standalone: true,
     templateUrl: './home.component.html',
