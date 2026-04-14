0de859edb846a8910db00781bf05b7af4e127142
pLinho <paulorettamozo@gmail.com>
2026-04-13 21:41:26 -0300

    ``` chore: Remove junit reporter from Karma configuration
    The 'junit' reporter has been removed from the 'reporters' array in `apps/karma.conf.js`. This modification simplifies the test reporting configuration by discontinuing the generation of JUnit XML reports, which are no longer utilized by our current CI/CD pipeline or required for local development processes. This change aims to streamline test execution and reduce superfluous output, focusing only on the reporters actively used for real-time feedback and necessary integrations.
    
    Sugestão para a próxima ação:
    Verificar a pipeline de CI/CD para confirmar que a remoção do reporter 'junit' não impactou negativamente a integração contínua ou a coleta de resultados de testes. Caso o projeto utilize alguma ferramenta de análise de cobertura de código ou histórico de testes que dependia explicitamente desses relatórios XML, é crucial garantir que elas continuem funcionando corretamente ou adaptar suas configurações.
    ```

--- apps/karma.conf.js
+++ apps/karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function (config) {
       ]
     },
     // O junit vive aqui, nos reporters!
-    reporters: ['progress', 'kjhtml', 'junit'],
+    reporters: ['progress', 'kjhtml'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
