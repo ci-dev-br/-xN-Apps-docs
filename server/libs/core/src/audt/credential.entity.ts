16aaf77edecb5ef12f79828a849375c674084536
pLinho <paulorettamozo@gmail.com>
2026-07-17 18:26:37 -0300

    Correção em encapsulamento do controle com request.send para controle do status da requisição no retorno junto da mensagem principal; Ajuste na propagação de erro; Retornado etratégia de refresh token para o storage do browser;

--- server/libs/core/src/audt/credential.entity.ts
+++ server/libs/core/src/audt/credential.entity.ts
@@ -14,7 +14,13 @@ export class Credential {
     @ApiProperty({ required: false }) @CreateDateColumn() createdAt?: Date;
     @ApiProperty({ required: false }) @ManyToMany(() => Application) @JoinTable() application?: Application;
     @ApiProperty({ required: false }) @Column({ nullable: true }) createdFromIp?: string;
+    /**
+     * A chave de acesso é considerada válida quando pode ser utilizada para gerar uma nova chave de acesso. A quantidade de chaves de acesso válidas é limitada segundo a política de segurança da aplicação. A chave de acesso é considerada inválida quando o usuário não pode gerar uma nova chave de acesso.
+     */
     @Column({ nullable: true, default: false }) valid?: boolean;
+    /**
+     * A chave de acesso é considerada viva quando o usuário está autenticado na aplicação.
+     */
     @Column({ nullable: true, default: false }) alive?: boolean;
     @Column({ nullable: true }) refreshToken?: string;
     @ApiProperty({ required: false, nullable: true })
